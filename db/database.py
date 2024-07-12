ce8be224a8517cd7d04ae924419ef4ddd391414a
morarubend <morarubend@gmail.com>
2024-07-11 20:45:47 -0400

    BBR1

--- db/database.py
+++ db/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = ('postgresql://rdmq:rdmq@localhost:5433/rdmq', echo=True)
 #engine = create_engine('postgresql://rdmq:rdmq@localhost:5433/clientes', echo=True)
-engine = create_engine('postgresql://rdmq:rdmq@db/clientes')
+#engine = create_engine('postgresql://rdmq:rdmq@db/clientes')
+engine = create_engine('postgresql://morabase_user:Ti4f7376sSV3ez4q9y56iqI8S4u8YoOI@dpg-cq85qsss1f4s73cfuc70-a/morabase')
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
